787825f0f25bd92a825f908d1f4e5db3670475fd
enchanting-process-7328 <peterlin5301997@gmail.com>
2019-06-18 00:01:58 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,29 @@
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  badgeArray = []
+  array.each do |name|
+    badgeArray.push(badge_maker(name))
+  end
+  return badgeArray
+end
+
+def assign_rooms(array)
+  speakerArray = []
+  array.each_with_index {|speaker, index|
+    speakerArray.push("Hello, #{speaker}! You'll be assigned to room #{index + 1}!")
+  }
+  return speakerArray
+end
+
+def printer(array)
+  for i in batch_badge_creator(array)
+    puts i
+  end
+  for i in assign_rooms(array)
+    puts i
+  end
+end
